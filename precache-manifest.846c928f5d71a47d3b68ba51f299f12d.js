4e8199c5f49ddf7f2fce14f743ac89362545668c
SashaZhardetskaya <oleksandra.zh@preply.com>
2021-03-25 00:04:53 +0200

    add files

---
+++ precache-manifest.846c928f5d71a47d3b68ba51f299f12d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1732b969ef1d819d50ab5b8363905849",
+    "url": "/fe-riddle-online-status/build/index.html"
+  },
+  {
+    "revision": "475b2cf998dd738c4d96",
+    "url": "/fe-riddle-online-status/build/static/css/2.1fe15dcd.chunk.css"
+  },
+  {
+    "revision": "84e18149860b02a50744",
+    "url": "/fe-riddle-online-status/build/static/css/main.c3f77910.chunk.css"
+  },
+  {
+    "revision": "475b2cf998dd738c4d96",
+    "url": "/fe-riddle-online-status/build/static/js/2.bd2c1c33.chunk.js"
+  },
+  {
+    "revision": "898a94c5cf5f038c6294fe2da31416cd",
+    "url": "/fe-riddle-online-status/build/static/js/2.bd2c1c33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84e18149860b02a50744",
+    "url": "/fe-riddle-online-status/build/static/js/main.db2629d8.chunk.js"
+  },
+  {
+    "revision": "cbd22e1f7a2ad76840cc",
+    "url": "/fe-riddle-online-status/build/static/js/runtime-main.b9af775b.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/fe-riddle-online-status/build/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/fe-riddle-online-status/build/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/fe-riddle-online-status/build/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/fe-riddle-online-status/build/static/media/notification.c0d3c94c.eot"
+  }
+]);
